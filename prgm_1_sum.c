b25d64a0399f064706271369aae9736eed9fe7e8
ANJELSUBAIR <anjelsubair@gmail.com>
2025-10-21 11:41:00 +0530

    Add files via upload

---
+++ prgm_1_sum.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+int a,b,s=0;
+printf("Enter the first number:");
+scanf("%d",&a);
+printf("Enter the second number:");
+scanf("%d",&b);
+s=a+b;
+printf("Sum of the numbers is: %d",s);
+
+}
